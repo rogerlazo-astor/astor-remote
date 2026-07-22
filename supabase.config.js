e1623adc7aff624b4403fc247e3a3eeb801389b8
rogerlazo-astor <rogerlazo@gmail.com>
2026-07-22 01:27:21 -0400

    config: add Supabase publishable key to ASTOR_CLOUD_CONFIG

--- supabase.config.js
+++ supabase.config.js
@@ -1,6 +1,6 @@
 window.ASTOR_CLOUD_CONFIG = {
   enabled: true,
-  supabaseUrl: "https://aimtrvetnzlomcnddumf.supabase.co",
-  publishableKey: "sb_publishable_bBprGKnwzTjPbla1TfndpQ_88I20iSU",
-  bucket: "astor-case-files",
+  supabaseUrl: 'https://aimtrvetnzlomcnddumf.supabase.co',
+  publishableKey: 'sb_publishable_bBprGKnwzTjPbla1TfndpQ_88I20iSU',
+  bucket: 'astor-case-files'
 };
